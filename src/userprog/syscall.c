1262735d5fac72a160b2295736f3e219acada907
Inho Kim <goodboyih96@gmail.com>
2018-10-05 10:05:45 -0700

    initial implementation of halt, exit, read system calls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -4,6 +4,9 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+#include "devices/shutdown.h"
+#include "devices/input.h"
+
 typedef int pid_t;
 
 static void syscall_handler (struct intr_frame *);
@@ -42,7 +45,6 @@ syscall_handler (struct intr_frame *f UNUSED)
   //printf ("system call!\n");
     switch(*((int*)f->esp)) {
         case SYS_HALT:
-            break;
         case SYS_EXIT:
             ((void (*) (void**)) syscall_ptr[*((int*) f->esp)]) (argv);
             break;
@@ -51,7 +53,6 @@ syscall_handler (struct intr_frame *f UNUSED)
         case SYS_WAIT:
             break;
         case SYS_READ:
-            break;
         case SYS_WRITE:
             f->eax = ((int (*) (void**)) syscall_ptr[*((int*) f->esp)]) (argv);
             //f->eax = write(STDOUT_FILENO, (const void*) *((uint32_t*) (f->esp + 2 * sizeof(uint32_t))), (unsigned) *((uint32_t*) (f->esp + 3 * sizeof(uint32_t))));
@@ -62,18 +63,34 @@ syscall_handler (struct intr_frame *f UNUSED)
   //thread_exit ();
 }
 
-static void halt (void **argv) {}
+static void halt (void **argv) {
+    shutdown_power_off();
+}
+
 static void exit (void **argv) {
     printf("%s: exit(%d)\n", thread_current()->name, *(int*)argv[1]);
     thread_exit ();
 }
+
 static pid_t exec (void **argv) {
     return 0;
 }
+
 static int wait (void **argv) {
     return -1;
 }
+
 static int read (void **argv) {
+    int i;
+    switch(*(int*)argv[1]) {
+        case STDIN_FILENO:
+            for(i = 0; i < *(int*)argv[3]; i++)
+                (*(char**)argv[2])[i] = input_getc();
+            return *(unsigned*)argv[3];
+            break;
+        default:
+            break;
+    }
     return 0;
 }
 
